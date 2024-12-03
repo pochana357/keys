0de9b6bc81589963774702c42e1ca8c834c37121
pochana357 <190461112+pochana357@users.noreply.github.com>
2024-12-03 18:58:47 +0900

    Add defensives

--- src/lib/api/defensiveData.ts
+++ src/lib/api/defensiveData.ts
@@ -1,40 +1,69 @@
 const defensiveData: number[] = [
-    8092, // Mind Blast
-    19236, // Desperate Player
-    586, // Fade
-    62618, // Power Word: Barrier
-    33206, // Pain Suppression
-    451235, // Voidwrath
+	// Priest
+	8092, // Mind Blast
+	19236, // Desperate Player
+	586, // Fade
+	62618, // Power Word: Barrier
+	33206, // Pain Suppression
+	451235, // Voidwrath
+	47536, // Rature
 
-    108281, // Ancestral Guidance
-    108271, // Astral Shift
-    108270, // Stone Bulwark Totem
-    198103, // Earth Elemental
+	// Shaman
+	108281, // Ancestral Guidance
+	108271, // Astral Shift
+	108270, // Stone Bulwark Totem
+	198103, // Earth Elemental
 
-    374227, // Zephyr
-    370665, // Rescue
-    406732, // Spatial Paradox
-    363916, // Obsidian Scale
-    374348, // Renewing Blaze
+	108280, // Healing Tide Totem
+	98008, // Spirit Link Totem
+	114052, // Ascendance
+	5394, // Healing Stream Totem
 
-    342245, // Alter Time
-    55342, // Mirror Image
-    110960, // Greater Invisibility
-    45438, // Ice Block
-    414660, // Mass Barrier,
+	// Evoker
+	374227, // Zephyr
+	370665, // Rescue
+	406732, // Spatial Paradox
+	363916, // Obsidian Scale
+	374348, // Renewing Blaze
 
-    204018, // Blessing of Spellwarding
-    6940, // Blessing of Sacrifice
-    432459, // Holy Bulwark
-    432472, // Sacred Weapon
-    642, // Divine Shield
+	// Death Knight
+	48707, // Anti-Magic Shell
+	49039, // Lichborne
+	48792, // Icebound Fortitude
+	51052, // Anti-Magic Zone
 
-    403876, // Divine Protection
-    184662, // Shield of Vengeance
+	// Mage
+	342245, // Alter Time
+	55342, // Mirror Image
+	110960, // Greater Invisibility
+	45438, // Ice Block
+	414660, // Mass Barrier
 
-    5487, // Bear Form
-    22812, // Barkskin
+	// Paladin
+	204018, // Blessing of Spellwarding
+	6940, // Blessing of Sacrifice
+	432459, // Holy Bulwark
+	432472, // Sacred Weapon
+	642, // Divine Shield
+	387174, // Eye of Tyr
+	86659, // Guardian of Ancient Kings
+	31850, // Ardent Defender
+	31884, // Avenging Wrath
 
-    431416, // Algari Healing Potion
+	403876, // Divine Protection
+	184662, // Shield of Vengeance
+
+	// Druid
+	5487, // Bear Form
+	22812, // Barkskin
+	22842, // Frenzied Regeneration
+
+	// Items
+	431416, // Algari Healing Potion
+	452767, // Heartseeking Health Injector
+
+	328404, // Discharged Anima
+	328406, // Discharged Anima
+	328050 // Discarded Shield
 ];
 export default defensiveData;
