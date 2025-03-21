f84be55dac41d7195059d8123e59412e2fda43e5
pochana357 <190461112+pochana357@users.noreply.github.com>
2025-03-21 19:53:24 +0900

    Remove skeleton from tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,13 @@
-import typography from '@tailwindcss/typography';
-import forms from '@tailwindcss/forms';
+// import typography from '@tailwindcss/typography';
+// import forms from '@tailwindcss/forms';
 import type { Config } from 'tailwindcss';
 
-import { skeleton, contentPath } from '@skeletonlabs/skeleton/plugin';
-import * as themes from '@skeletonlabs/skeleton/themes';
+import { contentPath } from '@skeletonlabs/skeleton/plugin';
 
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}', contentPath(import.meta.url, 'svelte')],
 	darkMode: 'selector',
 	theme: {
 		extend: {}
-	},
-
-	plugins: [
-		forms,
-		typography,
-		skeleton({
-			// NOTE: each theme included will be added to your CSS bundle
-			themes: [themes.cerberus]
-		})
-	]
+	}
 } satisfies Config;
